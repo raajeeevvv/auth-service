ab7dbe309272d1bf359169d41cdc2ca48620bdd6
Rajeevraj1234 <rarajeevraj@gmail.com>
2026-07-10 17:07:49 +0530

    init testing && setup done

---
+++ src/tests/health.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { jest, describe, expect, it, beforeAll, afterAll } from "@jest/globals";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import app from "../app";
+
+jest.mock("otplib", () => ({
+  authenticator: {
+    verify: jest.fn(),
+    generate: jest.fn(),
+  },
+}));
+
+let mongoServer: MongoMemoryServer;
+
+// runs ONCE before all tests in this file
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+// runs ONCE after all tests in this file — cleanup so that mongo instance don't become a zomie
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe("Dummy sanity test", () => {
+  it("should hit a route and get a response", async () => {
+    const response = await request(app).get("/health");
+    expect(response.status).toBeDefined();
+  });
+});
